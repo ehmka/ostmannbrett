1064d5928411f62ec9ffb67d7e99fa0e1ac79287
michael krausse <michael.krausse@sbb.ch>
2014-03-30 13:47:50 +0000

    first example

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ostmannbrett
 ============
 
+- install mpg123 => sudo apt-get install mpg123
+- in directory you need a file for each possible key (mapping to clip files is sensitive)
+-- example clips/A.mp3 will be played if you press A
+-- example clips/a,mp3 will be played if you press a 
+- Q and q will terminate the ostmannbrett
+- python ostmannbrett.py
---
+++ ostmannbrett.py
@@ -0,0 +1,16 @@
+from subprocess import call 
+import curses
+class OstmannBrett:
+
+   def convert(self, input):
+      return "clips/"+input + ".mp3"
+
+continueReading = True
+ostmann = OstmannBrett()
+stdscr = curses.initscr()
+while(continueReading):
+    inputA = chr(stdscr.getch())
+    if(inputA == 'Q'  or inputA == 'q'):
+       continueReading = False
+    else:
+       call(["mpg123", ostmann.convert(inputA)])
