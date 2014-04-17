c2f149f9aaa6e963a91aa437e17e49eaf3a3fa0a
rosi <pi@raspberrypi.(none)>
2014-04-17 21:19:57 +0000

    includes roland wishes

--- ostmannbrett.py
+++ ostmannbrett.py
@@ -1,33 +1,34 @@
-from subprocess import call 
+from subprocess import Popen 
 import curses
-import threading
+import sys
 
 class OstmannBrett:
 
    def convert(self, input):
       return "clips/"+input + ".mp3"
    
-   def foo(self):
-      print "foo"
-
    def runMusic(self,input):
-     print "HU"
-     call(["mpg123", self.convert(input)])
-
-
+     return Popen(["mpg123", self.convert(input)])
 
 continueReading = True
+laufende = [None, None, None]
+counter = 0
 
 def f(x):
     ostmann = OstmannBrett()
-    ostmann.runMusic(x)
+    return ostmann.runMusic(x)
 
 stdscr = curses.initscr()
 while(continueReading):
     inputA = chr(stdscr.getch())
-    if(inputA == 'Q'  or inputA == 'q'):
+    if(inputA == 'Q'):
+       laufende[0].terminate()
+       laufende[1].terminate()
+       laufende[2].terminate()
        curses.endwin()
        continueReading = False
     else:
-       thr = threading.Thread(target=f, args=(inputA), kwargs={})
-       thr.start()
+       if (laufende[counter] != None):
+           laufende[counter].terminate() 
+       laufende[counter] = f(inputA)
+       counter = (counter + 1) % 3
