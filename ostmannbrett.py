5c0af13f50d5d577e610d3f0a3dde8ea1ffbd464
rosi <pi@raspberrypi.(none)>
2014-04-17 20:31:36 +0000

    thread are started parallel

--- ostmannbrett.py
+++ ostmannbrett.py
@@ -1,15 +1,27 @@
 from subprocess import call 
 import curses
+import threading
+
 class OstmannBrett:
 
    def convert(self, input):
       return "clips/"+input + ".mp3"
    
+   def foo(self):
+      print "foo"
+
    def runMusic(self,input):
+     print "HU"
      call(["mpg123", self.convert(input)])
 
+
+
 continueReading = True
-ostmann = OstmannBrett()
+
+def f(x):
+    ostmann = OstmannBrett()
+    ostmann.runMusic(x)
+
 stdscr = curses.initscr()
 while(continueReading):
     inputA = chr(stdscr.getch())
@@ -17,4 +29,5 @@ while(continueReading):
        curses.endwin()
        continueReading = False
     else:
-       ostmann.runMusic(inputA)
+       thr = threading.Thread(target=f, args=(inputA), kwargs={})
+       thr.start()
